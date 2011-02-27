5746aab69dc667feccb2eef61811e5d3ec9395d8
windu2b <windu.2b@gmail.com>
2011-02-28 00:33:54 +0100

    add the functions

--- application/models/User.php
+++ application/models/User.php
@@ -1,8 +1,43 @@
 <?php
 class Application_Model_User extends Application_Model_AbstractModel
 {
+	protected $_login = '';
+	
+	
+	protected $_role;
+	
+	
+	
 	public function __construct( $options = array() )
 	{
 		parent::__construct( $options );
+	}
+
+	
+	public function setLogin( $login )
+	{
+		$this->_role = $role;
+		
+		return $this;
+	}
+	
+
+	public function getLogin()
+	{
+		return $this->_login;
+	}
+	
+	
+	public function setRole( $role = null )
+	{
+		$this->_role = $role;
+		
+		return $this;
+	}
+	
+	
+	public function getRole()
+	{
+		return $this->_role;
 	}
 }

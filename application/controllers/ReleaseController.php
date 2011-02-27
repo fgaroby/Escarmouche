2cefa7c42466b3c912aa9b1d6f43c5fa885432c1
windu2b <windu.2b@gmail.com>
2011-02-28 00:11:57 +0100

    * add 'displayAction' function ; * clean the 'init' function ; * add the '$_releaseMapper' property.

--- application/controllers/ReleaseController.php
+++ application/controllers/ReleaseController.php
@@ -10,31 +10,84 @@
 
 class ReleaseController extends Zend_Controller_Action
 {
-	private $_releaseTable;
-
+	protected $_releaseMapper;
+	
+	
 	public function init()
 	{
-		parent::init();
-
-		$this->_releaseTable = new Application_Model_Db_Table_Release();
-
-		// par d�faut un appel � render() annule le rendu automatique
-		// restauration du rendu via le helper viewRenderer.
-		// (cette action rend une vue)
-		$this->_helper->viewRenderer->setNoRender( false );
 		$this->view->setTitle( 'Releases' );
+		$this->_releaseMapper = new Application_Model_ReleaseMapper();
 	}
 
-
-	/**
-	 * The default action
-	 */
+	
 	public function indexAction()
 	{
 		$this->view->setTitle( 'Liste des releases de votre produit' );
 
 		$this->view->releases = $this->_releaseMapper->fetchAll();
+	}
+	
+	
+	public function displayAction()
+	{
+		$params = $this->getRequest()->getParams();
+		if( isset( $params['id'] ) )
+		{
+			$release = $this->_releaseMapper->find( $params['id' ] );
+			$this->view->release = $release;
+			$this->view->setTitle( $release->getName() );	
+		}
+		else
+			$this->_redirect( $this->view->url( array( 'controller' => 'release', 'action' => 'index' ) ), array( 'prependBase' => false ) );
+	}
+	
+	
+	public function editAction()
+	{
+		$params = $this->getRequest()->getParams();
+		$isUpdate = isset( $params['id'] ) && !empty( $params['id'] );
+		if( $isUpdate )
+		{
+			$params['id'] = (int ) $params['id'];
+			
+			$release = $this->_releaseMapper->find( $params['id'] );
+			if( $release === null )
+			{
+				$this->getRequest()->clearParams();
+				$this->_redirect( $this->view->url( array( 'controller' => 'release', 'action' => 'edit', 'id' => null ) ), array( 'prependBase' => false, 'code' => 303 ) );
+			}
+		}
+		else
+		{
+			$release = new Application_Model_Release( array( 'name' => 'default release' ) );
+		}
+		//Zend_Debug::dump($release);
+		$form = new Escarmouche_Form_Release( array( 'update' => $isUpdate ) );
+		$form->setAction( $this->view->link( 'release', 'edit', null, '', 'default', !$isUpdate ) )
+			 ->setMethod( 'post' )
+			 ->setDefaults( $release->toArray() );
 
-		$this->render();
+		// if $isUpdate, don't show the sprints fieldset
+		if( $isUpdate === true )
+		{
+			$form->removeElement( 'sprintRadio' );
+			$form->removeDisplayGroup( 'sprints' );
+		}
+			 
+		// Création des informations et ajout/suppression
+		if( $this->getRequest()->isPost() && $form->isValid( $_POST ) )
+		{
+			$values = $form->getValues();
+			// $values['creator'] = Zend_Auth::getInstance()->getIdentity()->id;
+			$release->setFromArray( array_intersect_key($values, $release->toArray() ) );
+
+			// Sauvegarde des informations
+			$this->_releaseMapper->save( $release );
+			
+			$this->_helper->FlashMessenger( "Insertion du produit '{$release->getName()}' effectuée ! " );
+			$this->_redirect( $this->view->url( array( 'controller' => 'release', 'action' => 'index' ) ), array( 'prependBase' => false ) );
+		}
+		
+		$this->view->form = $form;
 	}
 }

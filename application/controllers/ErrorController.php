bdd890a1bb59500a50f9e90ebe878f5184f5f6cf
windu2b <windu.2b@gmail.com>
2011-03-03 00:22:04 +0100

    Création des controllers 'Error' et 'Feature'

--- application/controllers/ErrorController.php
+++ application/controllers/ErrorController.php
@@ -33,19 +33,59 @@ class ErrorController extends Zend_Controller_Action
 			$this->view->setTitle( 'Erreur de l\'application' );
 			$this->view->message = $this->view->translate( 'Notre site est momentanément indisponible.' );
 		}
-		
+
 		// Vide le contenu de la réponse
 		$this->_response->clearBody();
-		
+
 		//Si mode debug
 		if( $this->getInvokeArg( 'debug' ) == 1 )
 		{
 			// Écrase le message, affiche l'exception complète
 			$this->view->message = $errors->exception;
 		}
-		
+
 		Zend_Registry::get( 'log' )->$log( $errors->exception );
+	}*/
+	
+	public function errorAction()
+	{
+		$errors = $this->_getParam('error_handler');
+
+		switch( $errors->type )
+		{
+			case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ROUTE:
+			case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
+			case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION:
+				// 404 error -- controller or action not found
+				$this->getResponse()->setHttpResponseCode(404);
+				$this->view->message = 'Page not found';
+			break;
+			
+			default:
+				// application error
+				$this->getResponse()->setHttpResponseCode(500);
+				$this->view->message = 'Application error';
+			break;
+		}
+
+		// Log exception, if logger available
+		if( $log = Zend_Registry::get( 'log' ) )
+		{
+			$exception = $errors->exception;
+			//$log->crit($this->view->message, $errors->exception );
+			$log->debug( $exception->getMessage()
+				. PHP_EOL
+				. $exception->getTraceAsString()
+				. PHP_EOL );
+		}
+
+		// conditionally display exceptions
+		if( $this->getInvokeArg('displayExceptions') == true )
+			$this->view->exception = $errors->exception;
+
+		$this->view->request = $errors->request;
 	}
+
 
 
 	/**

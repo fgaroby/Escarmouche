e5f40e72a5529bbaee937aaa214cf589d3e20484
windu2b <windu.2b@gmail.com>
2011-03-04 23:25:25 +0100

    Add the comment table class

---
+++ application/models/Db/Table/Comment.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ *  Application_Model_Db_Table_Comment
+ *  
+ *  LICENSE
+ *  
+ *  Copyright (C) 2011  windu.2b
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *  @author windu.2b
+ *  @license AGPL v3
+ *  @since 0.1
+ */
+
+class Application_Model_Db_Table_Comment extends Escarmouche_Db_Table_Abstract
+{
+	/**
+	 *
+	 * Table name
+	 * @var string
+	 */
+	protected $_name = 'comment';
+	
+	
+	protected $_referenceMap = array(	'Sprint'	=> array(	'columns'		=>'sprint',
+																'refTableClass'	=> 'Application_Model_Db_Table_Sprint',
+																'refColumns'	=> array( 'id' ) ),
+										'Story'		=> array(	'columns'		=>'story',
+																'refTableClass'	=> 'Application_Model_Db_Table_Story',
+																'refColumns'	=> array( 'id' ) ),
+										'Release'	=> array(	'columns'		=>'release',
+																'refTableClass'	=> 'Application_Model_Db_Table_Release',
+																'refColumns'	=> array( 'id' ) ) );
+}

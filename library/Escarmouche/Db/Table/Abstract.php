ac7f4b12186501e824532f1c537a3b4fb37c6682
windu2b <windu.2b@gmail.com>
2011-03-04 23:22:38 +0100

    add the 'update' function

--- library/Escarmouche/Db/Table/Abstract.php
+++ library/Escarmouche/Db/Table/Abstract.php
@@ -1,8 +1,21 @@
 <?php
+
+
 abstract class Escarmouche_Db_Table_Abstract extends Zend_Db_Table_Abstract
 {
 	protected $_primary = 'id';
 	
 	
 	protected $_rowClass = 'Escarmouche_Db_Table_Row_Abstract';
+	
+	
+	public function update( array $data, $where )
+	{
+		if( is_array( $where )
+			&& isset( $where['id = ?'] )
+			&& $where['id = ?'] instanceof Application_Model_AbstractModel )
+			$where['id = ?'] = $where['id = ?']->getId();
+			
+		return parent::update( $data, $where );
+	}
 }

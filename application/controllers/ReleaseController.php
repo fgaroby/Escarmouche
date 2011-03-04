7522b20993bf826c7bfcd93a6b5317dd1c0c675b
windu2b <windu.2b@gmail.com>
2011-03-04 23:21:59 +0100

    add the AGPL v3 license

--- application/controllers/ReleaseController.php
+++ application/controllers/ReleaseController.php
@@ -1,12 +1,28 @@
 <?php
-
 /**
- * ReleaseController
- *
- * @author Francescu Garoby
- * @version 0.1
+ *  ReleaseController
+ *  
+ *  LICENSE
+ *  
+ *  Copyright (C) 2011  windu.2b
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *  @author windu.2b
+ *  @license AGPL v3
+ *  @since 0.1
  */
-
 
 class ReleaseController extends Escarmouche_Controller_Abstract
 {
--- application/controllers/TaskController.php
+++ application/controllers/TaskController.php
@@ -1,11 +1,29 @@
 <?php
-
 /**
- * TaskController
- *
- * @author
- * @version
+ *  TaskController
+ *  
+ *  LICENSE
+ *  
+ *  Copyright (C) 2011  windu.2b
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *  @author windu.2b
+ *  @license AGPL v3
+ *  @since 0.1
  */
+
 class TaskController extends Escarmouche_Controller_Abstract
 {
 	protected $_taskMapper;
